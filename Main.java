e6755866dd82819e7488ff309540bda910fa1701
walk <98437818+w-a-lk-e-r@users.noreply.github.com>
2022-01-28 23:12:09 -0500

    Added hello message to code.

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@ package Israelvt;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello, my name is Israel");
+        System.out.println("Hello, my name is Israel");
+
+        System.out.println("Hello Israel, my name is Jason");
     }
 }
